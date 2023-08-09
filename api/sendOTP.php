6d7e501e7fbfb1cf034834f0bf8de63265ae24ab
s4hil <80034527+s4hil@users.noreply.github.com>
2023-08-10 00:18:27 +0530

    Update sendOTP.php

--- api/sendOTP.php
+++ api/sendOTP.php
@@ -8,7 +8,7 @@ include 'db.php';
 				sessionStorage.setItem("OTP", '<?php echo base64_encode($otp) ?>' );
 			</script>
 		<?php
-	    $url = "https://www.fast2sms.com/dev/bulkV2?authorization=bZgcqdzfKytpEBT53ulso4iWrY6PM82ULaj1eHh70DwRXxJANQin6uGfx8DEdbmtvCL25aMBAXJW1U4z&route=otp&variables_values=". $otp ."&flash=0&numbers=".$num;
+	    $url = "https://www.fast2sms.com/dev/bulkV2?authorization=&route=otp&variables_values=". $otp ."&flash=0&numbers=".$num;
 
 	    $ch = curl_init();
 	    $optArray = array(
